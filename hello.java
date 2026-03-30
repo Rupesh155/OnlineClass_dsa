568299450e99ea5149e0dc30b80fa4c3a14a83c3
Rupesh155 <rudhg35@gmail.com>
2026-03-30 20:56:18 +0530

    ArrayList

--- hello.java
+++ hello.java
@@ -1,18 +1,58 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-class hello{
+class  hello{
     public static void main(String[] args) {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        System.out.println(list);
+         ArrayList<Integer> list =new ArrayList<>();
+         list.add(20);
+         list.add(10);
+         list.add(30);
+        //  int max =list.get(0);
+        //  for(int i=1;i<list.size();i++){
 
-    //     int arr[]={1,2,3,4,5};
+        //     if(list.get(i)>max){
+        //         max=list.get(i);
+        //     }
+        //  }
+        //  System.out.println(max);
+         System.out.println(Collections.max(list));
+        //  Scanner  sc=new Scanner(System.in);
+        //  int n=sc.nextInt();
+        //  for(int i=0;i<n;i++){
+        //     int x=sc.nextInt();
+        //     list.add(x);
+        //  }
+        //  System.out.println(list);
+
+        //  list.add(10);
+        //  list.add(20);
+        //  list.add(30);
+        //  list.add(1,60);
+        //  System.out.println(list.get(2));
+        //  System.out.println(list.size());
+        //  list.set(1, 50);
+        //  System.out.println(list);
+
+        //  list.remove(0);
+        // //  list.clear();
+        //  System.out.println(list);
+        //  for(int i=0;i<list.size();i++){
+        //     System.out.println(list.get(i));
+        //  }
+
+           // c r u d
+
+
+      
+
+        //Array
+        // int a=10;
+        // int b=11;
+    //     int arr[]= {10,20,30,40,50};
     //    for(int i=0;i<arr.length;i++){
     //     System.out.print(arr[i]);
     //    }
+
     }
 }
