8d01132de4d205a93a0004e7caa761545671ec2c
Rupesh155 <rudhg35@gmail.com>
2026-03-30 19:43:09 +0530

    lect1

---
+++ hello.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+class hello{
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        System.out.println(list);
+
+    //     int arr[]={1,2,3,4,5};
+    //    for(int i=0;i<arr.length;i++){
+    //     System.out.print(arr[i]);
+    //    }
+    }
+}
